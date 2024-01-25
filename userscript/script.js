8c0f7994e49b9d3cf7dfbc7692b346e53353e405
drakeerv <drakeerv@outlook.com>
2024-01-25 17:50:29 -0500

    Add license to geofs

--- userscript/script.js
+++ userscript/script.js
@@ -7,6 +7,7 @@
 // @match        https://www.geo-fs.com/geofs.php?v=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function() {
